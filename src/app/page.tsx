54675faaab631def8737ee638974e6a1a379f8fa
sriram1604 <sriramvenkatesan1604@gmail.com>
2026-02-02 22:59:22 +0530

    06:back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
+"use client"
+
 import LogoutBtn from "@/components/predesigns/logoutBtn";
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils"
+import { useTRPC } from "@/trpc/client";
 import { caller } from "@/trpc/server";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 
-const page = async() => {
+const page = () => {
    
-  await requireAuth();
+  const trpc = useTRPC()
+  const queryClient = useQueryClient()
 
-  const data = await caller.getUsers();
+  const {data } = useQuery(trpc.getWorkflows.queryOptions())
+
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      queryClient.invalidateQueries(trpc.getWorkflows.queryOptions())
+    },
+    onError: () => {
+      console.log("error")
+    }
+  }))
 
   return (
     <div className='min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6'>
@@ -15,8 +30,9 @@ const page = async() => {
       protected page
 
       <div>
-        {JSON.stringify(data,null,2)}
+        {create.isPending ? "Creating..." : (create.error ? "Error" : JSON.stringify(data,null,2))}
       </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>Create</Button>
       <LogoutBtn />
     </div>
   )
